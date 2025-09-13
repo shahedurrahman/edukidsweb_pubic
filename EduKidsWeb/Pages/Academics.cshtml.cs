63903573c02833c3ac362509542c16bdcad88c71
Shahedur Rahman <shahed@live.com>
2025-09-14 01:57:23 +0600

    Academics.

---
+++ EduKidsWeb/Pages/Academics.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EduKidsWeb.Pages
+{
+    public class AcademicsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
